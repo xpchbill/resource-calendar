0dceb5feaab5108c63e43f840d4807554ec6f834
xpchbill <xpchbill8@gmail.com>
2015-12-24 17:02:09 +0800

    Format...

--- src/resource/day/ResourceDayGrid.js
+++ src/resource/day/ResourceDayGrid.js
@@ -1,40 +1,43 @@
 "use strict";
 
-import {DayGrid, htmlEscape} from "../FC.js";
+import {
+  DayGrid, htmlEscape
+}
+from "../FC.js";
 import ResourceGrid from "./ResourceGrid.js";
 
-export default class ResourceDayGrid extends DayGrid{
+export default class ResourceDayGrid extends DayGrid {
 
-	constructor(view){
-		super(view);
-		this.resourceCnt = 0;
-	}
+  constructor(view) {
+    super(view);
+    this.resourceCnt = 0;
+  }
 
-	/**
-	 * Render time scale HTML.
-	 * @override
-	 * @return {String}
-	 */
-	renderBgIntroHtml() {
-		var view = this.view;
+  /**
+   * Render time scale HTML.
+   * @override
+   * @return {String}
+   */
+  renderBgIntroHtml() {
+    var view = this.view;
 
-		return '' +
-			'<td class="fc-axis ' + view.widgetContentClass + '" ' + view.axisStyleAttr() + '>' +
-				'<span>' + // needed for matchCellWidths
-					(view.opt('allDayHtml') || htmlEscape(view.opt('allDayText'))) +
-				'</span>' +
-			'</td>';
-	}
+    return '' +
+      '<td class="fc-axis ' + view.widgetContentClass + '" ' + view.axisStyleAttr() + '>' +
+      '<span>' + // needed for matchCellWidths
+      (view.opt('allDayHtml') || htmlEscape(view.opt('allDayText'))) +
+      '</span>' +
+      '</td>';
+  }
 
-	/**
-	 * Render
-	 * @return {String}
-	 */
-	renderIntroHtml() {
-		return '<td class="fc-axis" ' + this.view.axisStyleAttr() + '></td>';
-	}
+  /**
+   * Render
+   * @return {String}
+   */
+  renderIntroHtml() {
+    return '<td class="fc-axis" ' + this.view.axisStyleAttr() + '></td>';
+  }
 
-	getHitSpan(hit) {
+  getHitSpan(hit) {
     var span;
     span = super.getHitSpan(hit);
     if (this.resourceCnt) {
